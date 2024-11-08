364cb0160a8f3866a30b36bfbea4c66b40cc58dd
Hector Vera <hector.vera.m@mail.pucv.cl>
2024-11-08 10:33:21 -0300

    ruta para movil

--- app/src/main/java/com/example/proyecto_de_titulo/interfazApiRest/RetrofitClient.kt
+++ app/src/main/java/com/example/proyecto_de_titulo/interfazApiRest/RetrofitClient.kt
@@ -7,7 +7,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 
 object RetrofitClient {
-    private const val BASE_URL = "https://easy-economy.fly.dev/"
+    private const val BASE_URL = "https://easy-economy.fly.dev/movil/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
